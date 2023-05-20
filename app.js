0e4e191cdda64520eee0beed3d9287190dee1ff3
Ahmad Shibbir <ahmadshibbir.iium@gmail.com>
2023-05-20 12:04:34 +0800

    established database connection with mongoose

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+// external import
 const express = require("express");
+const mongoose= require("mongoose");
 
+// internal import
 
+// express app initialization
 const app = express();
 app.use(express.json());
+
+// database connection with mongose 
+mongoose.connect("mongodb://localhost/todos", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+
+})
+    .then(()=>console.log("connection successful"))
+    .catch((err)=>console.log(err));
+
+// application routes
+
 
 // default error handler
 function errorHandler(err, req, res, next){
